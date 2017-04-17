093ca7d41a8379643c18e3a9bba8eac45591061c
ibrown <ibrown@twitter.com>
2017-04-16 23:24:20 -0700

    initial XML parser commit

---
+++ PhosPhfor/FlickrParser.h
@@ -0,0 +1,13 @@
+//
+//  FlickrParser.h
+//  PhosPhfor
+//
+//  Created by Ian Brown on 4/16/17.
+//  Copyright © 2017 Ian Brown. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlickrParser : NSObject
+
+@end
---
+++ PhosPhfor/FlickrParser.m
@@ -0,0 +1,13 @@
+//
+//  FlickrParser.m
+//  PhosPhfor
+//
+//  Created by Ian Brown on 4/16/17.
+//  Copyright © 2017 Ian Brown. All rights reserved.
+//
+
+#import "FlickrParser.h"
+
+@implementation FlickrParser
+
+@end
